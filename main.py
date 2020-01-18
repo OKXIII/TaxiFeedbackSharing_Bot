14bf04cb2e43587266f562c95fe62526a9c42458
OKXIII <kovchinnikov@gmail.com>
2020-01-18 17:31:28 +0300

    1

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ bot = telebot.TeleBot(config.token)
 
 #конвертация номеров в единый формат
 def convert_licenseplate(lp):
-    return u''.join([_trans_table.get(c,c) for c in lp])
-    pass
+    return u''.join([_trans_table.get(c,c) for c in lp.upper()])
 
 
 
