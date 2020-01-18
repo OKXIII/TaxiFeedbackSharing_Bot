3622f052843eeb355599eaca1aaec11f506bd918
OKXIII <kovchinnikov@gmail.com>
2020-01-18 16:30:16 +0300

    1

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # боевые токены
-#token= "1028605837:AAFtcyuoVRhyd17DT7WEWXlTH3Q5FCsc0YY"
+token= "1028605837:AAFtcyuoVRhyd17DT7WEWXlTH3Q5FCsc0YY"
 # тестовые токены
-token ="467904766:AAHqBaGfl0R4eEiBGPr2R48BONFlX2AQLEE"
+#token ="467904766:AAHqBaGfl0R4eEiBGPr2R48BONFlX2AQLEE"
 
 owner_chat_id=74869590
 mode="work"
