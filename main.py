f58bd8ce7bcf61350e27f81327ecd8e453bc4b45
OKXIII <kovchinnikov@gmail.com>
2020-01-21 10:45:50 +0300

    1

--- main.py
+++ main.py
@@ -93,7 +93,8 @@ def create_keyboard(type="common"):
         keyboard.add(button_yes, button_no)
     if type=="cancel":
         keyboard= types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-        button_yes = types.KeyboardButton(text="Отмена")
+        button_cancel = types.KeyboardButton(text="Отмена")
+        keyboard.add(button_cancel)
     if type=="null":
         keyboard= types.ReplyKeyboardRemove()
     return keyboard
@@ -106,41 +107,40 @@ def add_new_lp(message):
         bot.send_message(message.chat.id, "Добавляем новый номер")
         pass
     if config._REQUEST_TYPE == 2:
-        config._REQUEST_TYPE=0
         bot.send_message(message.chat.id, "Введите ваш отзыв о такси с номером {0}".format(config.LICENSEPLATE),reply_markup=create_keyboard("cancel"))
         pass
     return
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text==("Да"):
+    if message.text==("Да") and (config._REQUEST_TYPE==1 or config._REQUEST_TYPE==2):
         add_new_lp(message)
         return
-    if message.text==("Нет"):
+    if message.text==("Нет")  and (config._REQUEST_TYPE==1 or config._REQUEST_TYPE==2):
         request_lp(message)
         return
-    if message.text==("Отмена"):
+    if message.text==("Отмена")  and (config._REQUEST_TYPE==2):
         config._REQUEST_TYPE=0
         request_lp(message)
         return
 
-
-    m=convert_licenseplate(message.text).upper()
-    if not check_licenseplate_len(m):
-        bot.send_message(message.chat.id, "Недопустимая длина номера автомобиля. Пожалуйста укажите правильный номер.")
-        return
-    if not check_licenseplate_chars(m):
-        bot.send_message(message.chat.id, "В номере указаны неверные символы. Пожалуйста укажите правильный номер.")
-        return
-    config.LICENSEPLATE=m
-    r=get_info_lp(m)
-    if len(r)<1:
-        config._REQUEST_TYPE=1
-        bot.send_message(message.chat.id, "Информации по данному номеру нет. Хотите добавить?",reply_markup=create_keyboard())
-    else:
-  #Выводим информацию по номеру
-        config._REQUEST_TYPE=2
-        bot.send_message(message.chat.id, "Хотите добавить свой отзыв?",reply_markup=create_keyboard())
+    if  (config._REQUEST_TYPE==0):
+        m=convert_licenseplate(message.text).upper()
+        if not check_licenseplate_len(m):
+            bot.send_message(message.chat.id, "Недопустимая длина номера автомобиля. Пожалуйста укажите правильный номер.")
+            return
+        if not check_licenseplate_chars(m):
+            bot.send_message(message.chat.id, "В номере указаны неверные символы. Пожалуйста укажите правильный номер.")
+            return
+        config.LICENSEPLATE=m
+        r=get_info_lp(m)
+        if len(r)<1:
+            config._REQUEST_TYPE=1
+            bot.send_message(message.chat.id, "Информации по данному номеру нет. Хотите добавить?",reply_markup=create_keyboard())
+        else:
+      #Выводим информацию по номеру
+            config._REQUEST_TYPE=2
+            bot.send_message(message.chat.id, "Хотите добавить свой отзыв?",reply_markup=create_keyboard())
     pass
 
 
